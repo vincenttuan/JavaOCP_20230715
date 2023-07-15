d8ff2b2fc6c0907c183033aac07302d2e6c475cc
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 16:20:35 +0800

    java

--- JavaOCP_20230715/src/day01/DrinkMain7.java
+++ JavaOCP_20230715/src/day01/DrinkMain7.java
@@ -5,6 +5,11 @@ import static day01.DrinkMain5.calcDrinkSumAndPrint;
 public class DrinkMain7 {
 
 	public static void main(String[] args) {
+		
+		// 建立飲料名稱的陣列
+		String[] drinkNames = {"紅茶", "綠茶", "青茶", "巧克力牛奶", "木瓜牛奶", "拿鐵咖啡", "焦糖咖啡", "摩卡咖啡", "濃縮咖啡"};
+				
+		// 建立飲料
 		Drink d1 = new Drink("tea", "紅茶", 10);
 		Drink d2 = new Drink("tea", "綠茶", 12);
 		Drink d3 = new Drink("tea", "青茶", 15);
@@ -13,7 +18,9 @@ public class DrinkMain7 {
 		Drink d6 = new Drink("coffee", "拿鐵咖啡", 45);
 		Drink d7 = new Drink("coffee", "焦糖咖啡", 55);
 		Drink d8 = new Drink("coffee", "摩卡咖啡", 65);
+		Drink d9 = new Drink("coffee", "濃縮咖啡", 75);
 		
+		// 建立飲料單
 		Drink[] drinks = {
 				d1, d2, d3, d4, d5, d6, d7, d8,
 				d1, d1, d1, d2, d2, d3, d3, d4,
@@ -21,8 +28,6 @@ public class DrinkMain7 {
 				d1, d2, d3, d4, d5, d6, d7, d8,
 		};
 		
-		// 建立飲料名稱的陣列
-		String[] drinkNames = {"紅茶", "綠茶", "青茶", "巧克力牛奶", "木瓜牛奶", "拿鐵咖啡", "焦糖咖啡", "摩卡咖啡"};
 		
 		int total = 0; // 總金額
 		for(String drinkName : drinkNames) {
