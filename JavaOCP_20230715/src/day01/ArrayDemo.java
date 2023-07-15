0593e1b08f170e2bc5a67dc4c044b2cd8d2d57be
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:18:04 +0800

    java

---
+++ JavaOCP_20230715/src/day01/ArrayDemo.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 12;
+		int z = 15;
+		
+		int[] array = {x, y, z}; // 將 3 個變數放到陣列中
+		
+		System.out.println(array.length); // 陣列長度
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+		
+		System.out.println(array[0] + array[1] + array[2]);
+		
+		System.out.println("--------");
+		// 使用 for-loop
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+		
+	}
+
+}
