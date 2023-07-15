5823895eb62c4e6a1fd42b67fbfaf970b71e9239
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:31:24 +0800

    java

--- JavaOCP_20230715/src/day01/ArrayDemo2.java
+++ JavaOCP_20230715/src/day01/ArrayDemo2.java
@@ -1,14 +1,23 @@
 package day01;
 
+import java.util.Arrays;
+
 public class ArrayDemo2 {
 
 	public static void main(String[] args) {
 		int[] scores = {100, 40, 80, 70, 35};
-		// 印出所有成績
+		// 印出所有成績 for-loop
 		for(int i=0;i<scores.length;i++) {
 			System.out.println(scores[i]);
 		}
 		System.out.println("-------");
+		// 印出所有成績 for-each
+		for(int score : scores) {
+			System.out.println(score);
+		}
+		System.out.println("-------");
+		// 印出所有成績 Arrays.toString()
+		System.out.println(Arrays.toString(scores));
 
 	}
 
