f81f14525e035502c5c753fa9d9cfa145f3de7c3
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:28:49 +0800

    java

---
+++ JavaOCP_20230715/src/day01/ArrayDemo2.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		int[] scores = {100, 40, 80, 70, 35};
+		// 印出所有成績
+		for(int i=0;i<scores.length;i++) {
+			System.out.println(scores[i]);
+		}
+		System.out.println("-------");
+
+	}
+
+}
