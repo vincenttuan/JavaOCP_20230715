2e893371efd1c7ffab2f0aaff3dab61da161e9fd
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 12:19:45 +0800

    java

---
+++ JavaOCP_20230715/src/day01/PersonMain.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		p1.name = "Jack";
+		p1.sex = 'M';
+		p1.height = 170.0;
+		p1.weight = 60.0;
+		
+		Person p2 = new Person();
+		p2.name = "Rose";
+		p2.sex = 'F';
+		p2.height = 165.0;
+		p2.weight = 45.0;
+		
+	}
+
+}
