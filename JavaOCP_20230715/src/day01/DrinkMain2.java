d796457ee746c05d06601acc77158377079a491f
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:12:11 +0800

    java

---
+++ JavaOCP_20230715/src/day01/DrinkMain2.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class DrinkMain2 {
+
+	public static void main(String[] args) {
+		Drink d1 = new Drink("tea", "紅茶", 10);
+		Drink d2 = new Drink("tea", "綠茶", 12);
+		Drink d3 = new Drink("tea", "青茶", 15);
+		
+		
+		
+	}
+
+}
