5d40cbe76b233a1fe8db24253d5081cdd65b56ee
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:43:36 +0800

    java

--- JavaOCP_20230715/src/day01/DrinkMain2.java
+++ JavaOCP_20230715/src/day01/DrinkMain2.java
@@ -7,6 +7,16 @@ public class DrinkMain2 {
 		Drink d2 = new Drink("tea", "綠茶", 12);
 		Drink d3 = new Drink("tea", "青茶", 15);
 		
+		// 將 d1, d2, d3 放到陣列中
+		Drink[] drinks = {d1, d2, d3};
+		
+		for(int i=0;i<drinks.length;i++) {
+			System.out.println(drinks[i]);
+		}
+		System.out.println("-------------------------------------");
+		for(Drink drink : drinks) {
+			System.out.println(drink);
+		}
 		
 		
 	}
