e27434e3805bcaef933ae93be0cb20644c3e587a
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:56:09 +0800

    java

---
+++ JavaOCP_20230715/src/day01/DrinkMain3.java
@@ -0,0 +1,29 @@
+package day01;
+
+import java.util.Arrays;
+
+public class DrinkMain3 {
+
+	public static void main(String[] args) {
+		Drink d1 = new Drink("tea", "紅茶", 10);
+		Drink d2 = new Drink("tea", "綠茶", 12);
+		Drink d3 = new Drink("tea", "青茶", 15);
+		
+		// 客戶點餐
+		Drink[] drinks = {d1, d1, d2, d2, d2, d3, d3, d3, d3, d2, d2, d1};
+		
+		// 列印飲料單(逐筆印出每一筆飲料)
+		System.out.println("飲料單:");
+		System.out.println("-------------------------------------");
+		int sum = 0;
+		for(Drink drink : drinks) {
+			System.out.println(drink);
+			sum += drink.price;
+		}
+		System.out.println("-------------------------------------");
+		// 列印總金額
+		System.out.printf("總金額: $%d\n", sum);
+		
+	}
+
+}
