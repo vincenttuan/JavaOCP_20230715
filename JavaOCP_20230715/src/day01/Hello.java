08f77bf4b4ee4670badb33a36daefe42812e1851
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 10:47:12 +0800

    java

---
+++ JavaOCP_20230715/src/day01/Hello.java
@@ -0,0 +1,12 @@
+package day01;
+
+import java.util.Date;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("哈囉 Java !");
+		System.out.println(new Date());
+	}
+	
+}
