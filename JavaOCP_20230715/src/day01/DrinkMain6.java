5e7fe228ee24214cf1bf03939b81306fffef9c42
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 16:06:59 +0800

    java

--- JavaOCP_20230715/src/day01/DrinkMain6.java
+++ JavaOCP_20230715/src/day01/DrinkMain6.java
@@ -19,7 +19,19 @@ public class DrinkMain6 {
 				d1, d2, d3, d4, d5, d6, d7, d8,
 		};
 		
+		int sum1 = DrinkMain5.calcDrinkSumAndPrint("紅茶", drinks);
+		int sum2 = DrinkMain5.calcDrinkSumAndPrint("綠茶", drinks);
+		int sum3 = DrinkMain5.calcDrinkSumAndPrint("青茶", drinks);
+		int sum4 = DrinkMain5.calcDrinkSumAndPrint("巧克力牛奶", drinks);
+		int sum5 = DrinkMain5.calcDrinkSumAndPrint("木瓜牛奶", drinks);
+		int sum6 = DrinkMain5.calcDrinkSumAndPrint("拿鐵咖啡", drinks);
+		int sum7 = DrinkMain5.calcDrinkSumAndPrint("焦糖咖啡", drinks);
+		int sum8 = DrinkMain5.calcDrinkSumAndPrint("摩卡咖啡", drinks);
 		
+		System.out.println("--------------------------------------");
+		// 總金額
+		int total = sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8;
+		System.out.printf("總金額 %d 元\n", total);
 	}
 
 }
