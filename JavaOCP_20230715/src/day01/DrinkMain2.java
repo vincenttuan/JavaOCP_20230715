c49e3955c1e965170e33e2604e71a5e371311c08
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:45:51 +0800

    java

--- JavaOCP_20230715/src/day01/DrinkMain2.java
+++ JavaOCP_20230715/src/day01/DrinkMain2.java
@@ -10,13 +10,24 @@ public class DrinkMain2 {
 		// 將 d1, d2, d3 放到陣列中
 		Drink[] drinks = {d1, d2, d3};
 		
+		// 印出所有飲料
 		for(int i=0;i<drinks.length;i++) {
 			System.out.println(drinks[i]);
 		}
 		System.out.println("-------------------------------------");
+		
 		for(Drink drink : drinks) {
 			System.out.println(drink);
 		}
+		System.out.println("-------------------------------------");
+		
+		// 計算總和
+		int sum = 0;
+		for(int i=0;i<drinks.length;i++) {
+			sum += drinks[i].price;
+		}
+		System.out.printf("總和=%d\n", sum);
+		System.out.println("-------------------------------------");
 		
 		
 	}
