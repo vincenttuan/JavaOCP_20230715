b3f030df4a2c53449b8664e417a390b162b0dafc
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 16:10:15 +0800

    java

--- JavaOCP_20230715/src/day01/DrinkMain6.java
+++ JavaOCP_20230715/src/day01/DrinkMain6.java
@@ -1,5 +1,7 @@
 package day01;
 
+import static day01.DrinkMain5.calcDrinkSumAndPrint;
+
 public class DrinkMain6 {
 
 	public static void main(String[] args) {
@@ -19,14 +21,14 @@ public class DrinkMain6 {
 				d1, d2, d3, d4, d5, d6, d7, d8,
 		};
 		
-		int sum1 = DrinkMain5.calcDrinkSumAndPrint("紅茶", drinks);
-		int sum2 = DrinkMain5.calcDrinkSumAndPrint("綠茶", drinks);
-		int sum3 = DrinkMain5.calcDrinkSumAndPrint("青茶", drinks);
-		int sum4 = DrinkMain5.calcDrinkSumAndPrint("巧克力牛奶", drinks);
-		int sum5 = DrinkMain5.calcDrinkSumAndPrint("木瓜牛奶", drinks);
-		int sum6 = DrinkMain5.calcDrinkSumAndPrint("拿鐵咖啡", drinks);
-		int sum7 = DrinkMain5.calcDrinkSumAndPrint("焦糖咖啡", drinks);
-		int sum8 = DrinkMain5.calcDrinkSumAndPrint("摩卡咖啡", drinks);
+		int sum1 = calcDrinkSumAndPrint("紅茶", drinks);
+		int sum2 = calcDrinkSumAndPrint("綠茶", drinks);
+		int sum3 = calcDrinkSumAndPrint("青茶", drinks);
+		int sum4 = calcDrinkSumAndPrint("巧克力牛奶", drinks);
+		int sum5 = calcDrinkSumAndPrint("木瓜牛奶", drinks);
+		int sum6 = calcDrinkSumAndPrint("拿鐵咖啡", drinks);
+		int sum7 = calcDrinkSumAndPrint("焦糖咖啡", drinks);
+		int sum8 = calcDrinkSumAndPrint("摩卡咖啡", drinks);
 		
 		System.out.println("--------------------------------------");
 		// 總金額
