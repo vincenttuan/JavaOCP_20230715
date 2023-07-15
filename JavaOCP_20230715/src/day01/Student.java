689661b4386d75556a2100dd77cccaca043a5c39
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 12:06:20 +0800

    java

--- JavaOCP_20230715/src/day01/Student.java
+++ JavaOCP_20230715/src/day01/Student.java
@@ -4,8 +4,12 @@ public class Student { // 學生
 	String name; // 姓名
 	int score;   // 成績
 	
-	boolean isPass() {
+	boolean isPass() { // 判斷是否及格
 		boolean pass = score > 60; // 判斷成績是否大於 60
 		return pass; // 將結果回傳
+	}
+	
+	void printResult() { // 印出結果
+		System.out.printf("姓名:%s 分數:%d %s\n", name, score, (isPass()?"及格":"不及格 還差 " + (60-score) + " 分"));
 	}
 }
--- JavaOCP_20230715/src/day01/StudentMain.java
+++ JavaOCP_20230715/src/day01/StudentMain.java
@@ -23,6 +23,9 @@ public class StudentMain {
 		// 若是不及格要印出還差幾分 ?
 		System.out.printf("姓名:%s 分數:%d %s\n", s1.name, s1.score, (s1.isPass()?"及格":"不及格 還差 " + (60-s1.score) + " 分"));
 		System.out.printf("姓名:%s 分數:%d %s\n", s2.name, s2.score, (s2.isPass()?"及格":"不及格 還差 " + (60-s2.score) + " 分"));
+	
+		s1.printResult();
+		s2.printResult();
 	}
 
 }
