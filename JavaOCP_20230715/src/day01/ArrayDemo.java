6281078dcfc8114c1f20c724c031b1dc09a8473c
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-15 14:21:46 +0800

    java

--- JavaOCP_20230715/src/day01/ArrayDemo.java
+++ JavaOCP_20230715/src/day01/ArrayDemo.java
@@ -10,13 +10,14 @@ public class ArrayDemo {
 		int[] array = {x, y, z}; // 將 3 個變數放到陣列中
 		
 		System.out.println(array.length); // 陣列長度
+		System.out.println("--------");
+		
 		System.out.println(array[0]);
 		System.out.println(array[1]);
 		System.out.println(array[2]);
-		
 		System.out.println(array[0] + array[1] + array[2]);
-		
 		System.out.println("--------");
+		
 		// 使用 for-loop
 		int sum = 0; // 總和
 		for(int i=0;i<array.length;i++) {
