2494a4755788ceaf00d0421e7f6e07c2e90050ff
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-22 13:47:39 +0800

    java

---
+++ JavaOCP_20230715/src/day02/Student.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class Student {
+	public final String NAME;
+	public final String SCORE1;
+	public final String SCORE2;
+	public final String SCORE3;
+	
+	public Student(String nAME, String sCORE1, String sCORE2, String sCORE3) {
+		super();
+		NAME = nAME;
+		SCORE1 = sCORE1;
+		SCORE2 = sCORE2;
+		SCORE3 = sCORE3;
+	}
+	
+}
