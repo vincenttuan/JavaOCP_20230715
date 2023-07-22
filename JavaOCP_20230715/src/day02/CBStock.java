2e2f6a2ea313a4ff493125841546fab4a447db29
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-22 10:48:11 +0800

    java

--- JavaOCP_20230715/src/day02/CBStock.java
+++ JavaOCP_20230715/src/day02/CBStock.java
@@ -1,6 +1,7 @@
 package day02;
 
 public class CBStock {
+	//-- 原始資料 -------------------------------------
 	public final String CB_TRAN_DATE;
 	public final String CB_BROKER_ID;
 	public final String CB_ACNO;
@@ -9,6 +10,8 @@ public class CBStock {
 	public final String CB_UNSOLD_STOCK_NOS;
 	public final String CB_UNSOLD_COST;
 	public final String CB_NOCOST_STOCK_NOS;
+	//-- 自訂資料 -------------------------------------
+	public final String TOTAL_STOCK_NOS;
 	
 	public CBStock(String cB_TRAN_DATE, String cB_BROKER_ID, String cB_ACNO, String cB_COMPANY_NO, String cB_DEPT,
 			String cB_UNSOLD_STOCK_NOS, String cB_UNSOLD_COST, String cB_NOCOST_STOCK_NOS) {
@@ -20,6 +23,8 @@ public class CBStock {
 		CB_UNSOLD_STOCK_NOS = cB_UNSOLD_STOCK_NOS;
 		CB_UNSOLD_COST = cB_UNSOLD_COST;
 		CB_NOCOST_STOCK_NOS = cB_NOCOST_STOCK_NOS;
+		// 計算總庫存 (CB_UNSOLD_STOCK_NOS + CB_NOCOST_STOCK_NOS)
+		TOTAL_STOCK_NOS = String.valueOf(Integer.parseInt(CB_UNSOLD_STOCK_NOS) + Integer.parseInt(CB_NOCOST_STOCK_NOS));
 	}
 
 	@Override
@@ -27,8 +32,8 @@ public class CBStock {
 		return "CBStock [CB_TRAN_DATE=" + CB_TRAN_DATE + ", CB_BROKER_ID=" + CB_BROKER_ID + ", CB_ACNO=" + CB_ACNO
 				+ ", CB_COMPANY_NO=" + CB_COMPANY_NO + ", CB_DEPT=" + CB_DEPT + ", CB_UNSOLD_STOCK_NOS="
 				+ CB_UNSOLD_STOCK_NOS + ", CB_UNSOLD_COST=" + CB_UNSOLD_COST + ", CB_NOCOST_STOCK_NOS="
-				+ CB_NOCOST_STOCK_NOS + "]";
+				+ CB_NOCOST_STOCK_NOS + ", TOTAL_STOCK_NOS=" + TOTAL_STOCK_NOS + "]";
 	}
-	
+
 	
 }
