a078148963bb44d12baaab2dc5619f76751a368a
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-22 12:07:21 +0800

    java

--- JavaOCP_20230715/src/day02/StringDemo2.java
+++ JavaOCP_20230715/src/day02/StringDemo2.java
@@ -3,6 +3,7 @@ package day02;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringDemo2 {
@@ -31,6 +32,43 @@ public class StringDemo2 {
 		System.out.println("第2筆:"  + rows[1]);
 		System.out.println("第10筆:" + rows[9]);
 		System.out.println("最末筆:" + rows[rows.length - 1]);
+		// 印出 rows 全部資料
+		System.out.println("全部資料:" + Arrays.toString(rows));
+		
+		System.out.println("------------------------------------------------------------------------------------");
+		//------------------------------------------------------------------------------------------------------
+		// 將 rows 中的每一筆 data 轉 CBStock 物件以利後續分析
+		for(String data : rows) {
+			final String CB_TRAN_DATE        = data.substring(0, 0+7);
+			final String CB_BROKER_ID        = data.substring(7, 7+4);
+			final String CB_ACNO             = data.substring(11, 11+7);
+			final String CB_COMPANY_NO       = data.substring(18, 18+6);
+			final String CB_DEPT             = data.substring(24, 24+1);
+			final String CB_UNSOLD_STOCK_NOS = data.substring(25, 25+12);
+			final String CB_UNSOLD_COST      = data.substring(37, 37+14);
+			final String CB_NOCOST_STOCK_NOS = data.substring(51, 51+12);
+			// 將資料配置到 CBStock 物件中
+			CBStock cbStock = new CBStock(CB_TRAN_DATE, CB_BROKER_ID, CB_ACNO, CB_COMPANY_NO, CB_DEPT, CB_UNSOLD_STOCK_NOS, CB_UNSOLD_COST, CB_NOCOST_STOCK_NOS);
+			System.out.println(cbStock);
+		}
+		
+		System.out.println("------------------------------------------------------------------------------------");
+		
+		// 將 rows 中的每一筆 data 轉 CBStock 物件以利後續分析
+		for(int i=0;i<rows.length;i++) {
+			String data = rows[i];
+			final String CB_TRAN_DATE        = data.substring(0, 0+7);
+			final String CB_BROKER_ID        = data.substring(7, 7+4);
+			final String CB_ACNO             = data.substring(11, 11+7);
+			final String CB_COMPANY_NO       = data.substring(18, 18+6);
+			final String CB_DEPT             = data.substring(24, 24+1);
+			final String CB_UNSOLD_STOCK_NOS = data.substring(25, 25+12);
+			final String CB_UNSOLD_COST      = data.substring(37, 37+14);
+			final String CB_NOCOST_STOCK_NOS = data.substring(51, 51+12);
+			// 將資料配置到 CBStock 物件中
+			CBStock cbStock = new CBStock(CB_TRAN_DATE, CB_BROKER_ID, CB_ACNO, CB_COMPANY_NO, CB_DEPT, CB_UNSOLD_STOCK_NOS, CB_UNSOLD_COST, CB_NOCOST_STOCK_NOS);
+			System.out.println(cbStock);
+		}
 		
 		
 		
