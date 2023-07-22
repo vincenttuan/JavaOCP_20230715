c7f4011d7cc4a788c06aa29fc24f6f1508ae12d2
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-22 16:40:55 +0800

    java

--- JavaOCP_20230715/src/day02/StringDemo6.java
+++ JavaOCP_20230715/src/day02/StringDemo6.java
@@ -2,8 +2,12 @@ package day02;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
 import com.google.gson.Gson;
 
@@ -44,6 +48,27 @@ public class StringDemo6 {
 			}
 		}
 		
+		// SCORE1, SCORE2, SCORE3 相關分析
+		List<Double> score1s = new ArrayList<>();
+        List<Double> score2s = new ArrayList<>();
+        List<Double> score3s = new ArrayList<>();
+
+        for (Student student : students) {
+            score1s.add(Double.parseDouble(student.SCORE1));
+            score2s.add(Double.parseDouble(student.SCORE2));
+            score3s.add(Double.parseDouble(student.SCORE3));
+        }
+
+        double[] score1Array = score1s.stream().mapToDouble(d->d).toArray();
+        double[] score2Array = score2s.stream().mapToDouble(d->d).toArray();
+        double[] score3Array = score3s.stream().mapToDouble(d->d).toArray();
+
+        PearsonsCorrelation pc = new PearsonsCorrelation();
+        // correlation 相關係數
+        // 0.3: 弱相關, 0.45: 中度相關, 0.7: 高度相關
+        System.out.println("SCORE1 與 SCORE2 的相關性: " + pc.correlation(score1Array, score2Array));
+        System.out.println("SCORE1 與 SCORE3 的相關性: " + pc.correlation(score1Array, score3Array));
+        System.out.println("SCORE2 與 SCORE3 的相關性: " + pc.correlation(score2Array, score3Array));
 	}
 
 }
