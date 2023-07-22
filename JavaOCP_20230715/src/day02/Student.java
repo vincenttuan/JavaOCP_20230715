a35598c6575488c7230b33ca7e7fed3b0b7cc991
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-22 16:19:21 +0800

    java

--- JavaOCP_20230715/src/day02/Student.java
+++ JavaOCP_20230715/src/day02/Student.java
@@ -13,6 +13,14 @@ public class Student {
 		SCORE2 = sCORE2;
 		SCORE3 = sCORE3;
 	}
+	
+	public boolean isImproving() {
+		int score1 = Integer.parseInt(SCORE1);
+		int score2 = Integer.parseInt(SCORE2);
+		int score3 = Integer.parseInt(SCORE3);
+		
+		return score1 < score2 && score2 < score3;
+	}
 
 	@Override
 	public String toString() {
