07808ce428099404ba1328891126e2a4b592bd25
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-29 10:43:44 +0800

    java

---
+++ JavaOCP_20230715/src/day03/ArrayDemo5.java
@@ -0,0 +1,24 @@
+package day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo5 {
+	
+	// 陣列資料過濾
+	public static void main(String[] args) {
+		// 有一陣列 arr: [1, 2, 3, 4, 5]
+		int[] arr = {1, 2, 3, 4, 5};
+		System.out.println("arr:" + Arrays.toString(arr));
+		// 我希望將 arr 中的偶數拿掉使 arr: [1, 3, 5]
+		// 步驟 1: 先計算出奇數有幾個
+		
+		// 步驟 2: 根據奇數的數量創造新的陣列 newArr
+		
+		// 步驟 3: 利用 for-loop 或 for-each 將 arr 中的奇數依序放入到新陣列 newArr
+		
+		// 步驟 4: arr 指向新陣列 newArr
+		
+
+	}
+
+}
