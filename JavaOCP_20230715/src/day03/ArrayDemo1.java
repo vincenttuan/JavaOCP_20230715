82b08d9d990f3e8a28340bee47d07998bcb7739b
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-29 10:13:24 +0800

    java

--- JavaOCP_20230715/src/day03/ArrayDemo1.java
+++ JavaOCP_20230715/src/day03/ArrayDemo1.java
@@ -9,10 +9,12 @@ public class ArrayDemo1 {
 		int[] scores = new int[3];
 		System.out.println("[" + scores[0] + ", " + scores[1] + ", " + scores[2] + "]");
 		System.out.println(Arrays.toString(scores));
+		System.out.println("陣列長度:" + scores.length);
 		// 修改維度內容
 		scores[0] = 100;
 		scores[1] = 90;
 		scores[2] = 80;
+		//scores[3] = 70; // 超過 index 範圍
 		System.out.println("[" + scores[0] + ", " + scores[1] + ", " + scores[2] + "]");
 		System.out.println(Arrays.toString(scores));
 		
