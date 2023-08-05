14448c78620e6fa8e5875ae05522bc43d066e546
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 12:17:37 +0800

    java

---
+++ JavaOCP_20230715/src/day04/ProductPromotion5.java
@@ -0,0 +1,15 @@
+package day04;
+
+/*
+ * 仿造 ProductPromotion.java
+ * 商品促銷
+ * 假設 A 飲料 一瓶 20 元
+ * 第二件 6 折
+ * 請問買 11 瓶 A 飲料要多少錢 ?
+ * */
+
+public class ProductPromotion5 {
+	public static void main(String[] args) {
+		
+	}
+}
