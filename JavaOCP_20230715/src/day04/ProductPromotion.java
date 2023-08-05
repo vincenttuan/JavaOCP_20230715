28b8c0ab48053a69d03b563316a889167d6c81a8
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 10:23:24 +0800

    java

--- JavaOCP_20230715/src/day04/ProductPromotion.java
+++ JavaOCP_20230715/src/day04/ProductPromotion.java
@@ -3,19 +3,22 @@ package day04;
 /*
  * 商品促銷
  * 假設 A 飲料 一瓶 20 元
- * 買 3 送 1
+ * 買 5 送 2
  * 請問買 22 瓶 A 飲料要多少錢 ?
  * */
 public class ProductPromotion {
 
 	public static void main(String[] args) {
 		int bottlesToBuy = 22; // 總共要買的數量
+		
 		int pricePerBottle = 20; // A飲料每瓶價格
+		int buyNumber = 3; // 買的數量
+		int freeNumber = 1; // 送的數量
 		
 		// 每一組的價格相加總
-		int groupSize = 4;
+		int groupSize = buyNumber + freeNumber;
 		int groupAmount = bottlesToBuy / groupSize;
-		int bottlesToPayForInGroups = groupAmount * (3 * pricePerBottle);
+		int bottlesToPayForInGroups = groupAmount * (buyNumber * pricePerBottle);
 		
 		// 剩餘加總
 		int remainingAmount = bottlesToBuy % groupSize; // 剩餘數量
