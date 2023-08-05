1a1f8137d2f55f806a26f27dcf65769bf9baf498
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 12:28:02 +0800

    java

--- JavaOCP_20230715/src/day04/ProductPromotion5.java
+++ JavaOCP_20230715/src/day04/ProductPromotion5.java
@@ -14,6 +14,20 @@ public class ProductPromotion5 {
         int pricePerBottle = 20; // 每瓶飲料的價格
         double discount = 0.6; // 第二件的折扣率
         
-        
+        // 每一組的價格相加總
+        int groupSize = 2;
+        int groupAmount = bottlesToBuy / groupSize;
+        int bottlesToPayForInGroups = (int)(groupAmount * ((1+discount) * pricePerBottle));
+		
+        // 剩餘加總
+     	int remainingAmount = bottlesToBuy % groupSize; // 剩餘數量
+     	int remainingBottlesToPayFor = remainingAmount * pricePerBottle; // 剩餘數量要付的費用 
+     		
+     	// 總計 
+     	int totalPrice = bottlesToPayForInGroups + remainingBottlesToPayFor;
+     	
+     	System.out.printf("A 飲料一瓶 %d 元(第二件 %.0f 折), 買 %d 瓶共需 %d 元\n", 
+				pricePerBottle, discount*10, bottlesToBuy, totalPrice);
+		
 	}
 }
