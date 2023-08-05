41c7640669aac16754e35c3e3d87334e0cdcd911
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 16:00:45 +0800

    java

---
+++ JavaOCP_20230715/src/day04/ExamScore.java
@@ -0,0 +1,51 @@
+package day04;
+
+public class ExamScore {
+	private int score1;
+	private int score2;
+	private int score3;
+	private int score4;
+	
+	public ExamScore(int score1, int score2, int score3, int score4) {
+		super();
+		this.score1 = score1;
+		this.score2 = score2;
+		this.score3 = score3;
+		this.score4 = score4;
+	}
+
+	public int getScore1() {
+		return score1;
+	}
+
+	public void setScore1(int score1) {
+		this.score1 = score1;
+	}
+
+	public int getScore2() {
+		return score2;
+	}
+
+	public void setScore2(int score2) {
+		this.score2 = score2;
+	}
+
+	public int getScore3() {
+		return score3;
+	}
+
+	public void setScore3(int score3) {
+		this.score3 = score3;
+	}
+
+	public int getScore4() {
+		return score4;
+	}
+
+	public void setScore4(int score4) {
+		this.score4 = score4;
+	}
+	
+	
+	
+}
