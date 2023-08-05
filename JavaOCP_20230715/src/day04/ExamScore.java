02184db5c8e72b2eb0f9754fa1859eb728be2e74
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 16:22:47 +0800

    java

--- JavaOCP_20230715/src/day04/ExamScore.java
+++ JavaOCP_20230715/src/day04/ExamScore.java
@@ -7,7 +7,6 @@ public class ExamScore {
 	private int score4;
 	
 	public ExamScore(int score1, int score2, int score3, int score4) {
-		super();
 		this.score1 = score1;
 		this.score2 = score2;
 		this.score3 = score3;
