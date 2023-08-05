9105d68c7d97b8d04047781f27b21b3c8f5945c2
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 10:57:51 +0800

    java

---
+++ JavaOCP_20230715/src/day04/Drink.java
@@ -0,0 +1,30 @@
+package day04;
+
+public class Drink {
+	private String name;
+	private int pricePerBottle;
+	
+	public Drink(String name, int pricePerBottle) {
+		this.name = name;
+		this.pricePerBottle = pricePerBottle;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPricePerBottle() {
+		return pricePerBottle;
+	}
+
+	public void setPricePerBottle(int pricePerBottle) {
+		this.pricePerBottle = pricePerBottle;
+	}
+	
+	
+	
+}
