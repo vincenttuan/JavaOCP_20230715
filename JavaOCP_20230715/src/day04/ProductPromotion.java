f4b3b022f5a0b1f98fd1fa5f3581f6204575b3d3
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-05 10:19:49 +0800

    java

--- JavaOCP_20230715/src/day04/ProductPromotion.java
+++ JavaOCP_20230715/src/day04/ProductPromotion.java
@@ -4,7 +4,7 @@ package day04;
  * 商品促銷
  * 假設 A 飲料 一瓶 20 元
  * 買 3 送 1
- * 請問買 22 瓶要多少錢 ?
+ * 請問買 22 瓶 A 飲料要多少錢 ?
  * */
 public class ProductPromotion {
 
@@ -12,7 +12,18 @@ public class ProductPromotion {
 		int bottlesToBuy = 22; // 總共要買的數量
 		int pricePerBottle = 20; // A飲料每瓶價格
 		
-
+		// 每一組的價格相加總
+		int groupSize = 4;
+		int groupAmount = bottlesToBuy / groupSize;
+		int bottlesToPayForInGroups = groupAmount * (3 * pricePerBottle);
+		
+		// 剩餘加總
+		int remainingAmount = bottlesToBuy % groupSize; // 剩餘數量
+		int remainingBottlesToPayFor = remainingAmount * pricePerBottle; // 剩餘數量要付的費用 
+		
+		// 總計 
+		int totalPrice = bottlesToPayForInGroups + remainingBottlesToPayFor;
+		System.out.printf("總價 $%d\n", totalPrice);
 	}
 
 }
