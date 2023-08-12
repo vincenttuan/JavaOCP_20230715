367d055521e7cd7ac565f0e9d31324d2b0ad0d3f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 15:55:38 +0800

    java

--- JavaOCP_20230715/src/day06/FoodTest.java
+++ JavaOCP_20230715/src/day06/FoodTest.java
@@ -5,7 +5,7 @@ public class FoodTest {
 	public static void main(String[] args) {
 		// 設定主餐
 		MainCourse noodles = new MainCourse("排骨麵", 115, "炸");
-		MainCourse rice = new MainCourse("雞腿飯", 128, "魯");
+		MainCourse rice = new MainCourse("雞腿飯", 128, "滷");
 		// 設定附餐
 		SideDish soup = new SideDish("玉米湯", 50, false);
 		SideDish tea = new SideDish("紅茶", 20, true);
