f9d2492886df5148b4725f4c26d8508cb884c603
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 10:27:11 +0800

    java

--- JavaOCP_20230715/src/day05/BreadTest.java
+++ JavaOCP_20230715/src/day05/BreadTest.java
@@ -1,9 +1,14 @@
 package day05;
 
+// 測試 Bread 的程式
 public class BreadTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Bread bread1 = new Bread();
+		bread1.name = "冰火波羅麵包";
+		bread1.price = 50;
+		bread1.calories = 100.5;
+		
 
 	}
 
