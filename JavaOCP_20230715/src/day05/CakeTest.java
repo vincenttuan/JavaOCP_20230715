c4fcbd89ecf7e453e2b89813645023ec6d0e88e3
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 10:48:54 +0800

    java

---
+++ JavaOCP_20230715/src/day05/CakeTest.java
@@ -0,0 +1,17 @@
+package day05;
+
+//測試 Cake 的程式
+public class CakeTest {
+
+	public static void main(String[] args) {
+		Cake cake1 = new Cake();
+		cake1.setName("巧克力蛋糕");
+		cake1.setPrice(65);
+		cake1.setSweetRating(30);
+		
+		
+		
+
+	}
+
+}
