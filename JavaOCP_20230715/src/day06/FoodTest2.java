750fe7b88f89f3033469a8b43ccb4bb426173e6b
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 16:25:54 +0800

    java

---
+++ JavaOCP_20230715/src/day06/FoodTest2.java
@@ -0,0 +1,43 @@
+package day06;
+
+public class FoodTest2 {
+
+	public static void main(String[] args) {
+		// 設定主餐
+		MainCourse noodles = new MainCourse("排骨麵", 115, "炸");
+		MainCourse rice = new MainCourse("雞腿飯", 128, "滷");
+		// 設定附餐
+		SideDish soup = new SideDish("玉米湯", 50, false);
+		SideDish tea = new SideDish("紅茶", 20, true);
+		
+		// 點餐資料
+		Food[] foods = {noodles, rice, soup, tea};
+		
+		// 印出餐點明細資料
+		displayBillDetails(foods);
+	}
+	
+	// 顯示明細
+	public static void displayBillDetails(Food[] foods) {
+		System.out.println("======================= 結帳明細 =======================");
+		System.out.printf("%-20s %-4s\n", "品名", "價格");
+		System.out.println("------------------------------------------------------");
+		
+		int subtotal = 0;
+		for(Food food : foods) {
+			System.out.printf("%-20s $%-4d\n", food.getName(), food.getPrice());
+			subtotal += food.getPrice();
+		}
+		System.out.println("------------------------------------------------------");
+		System.out.printf("%-20s $%-4d\n", "小計", subtotal);
+		
+		int tax = (int)(subtotal * 0.05); // 加上 5% 的稅
+		System.out.printf("%-20s $%-4d\n", "稅", tax);
+		
+		int total = subtotal + tax;
+		System.out.printf("%-20s $%-4d\n", "總計", total);
+		System.out.println("======================================================");
+	}
+	
+
+}
