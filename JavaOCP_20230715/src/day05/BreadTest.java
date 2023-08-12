30824e583a5b4883a8569e28b3aa1f0ca95e1e4f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 10:35:39 +0800

    java

--- JavaOCP_20230715/src/day05/BreadTest.java
+++ JavaOCP_20230715/src/day05/BreadTest.java
@@ -5,10 +5,17 @@ public class BreadTest {
 
 	public static void main(String[] args) {
 		Bread bread1 = new Bread();
-		bread1.name = null;
-		bread1.price = -50;
-		bread1.calories = 100000000.5;
+		bread1.setName("冰火菠羅麵包");
+		bread1.setPrice(50);
+		bread1.setCalories(100.5);
 		
+		Bread bread2 = new Bread();
+		bread2.setName("熱狗麵包");
+		bread2.setPrice(45);
+		bread2.setCalories(88.5);
+		
+		bread1.display();
+		bread2.display();
 
 	}
 
