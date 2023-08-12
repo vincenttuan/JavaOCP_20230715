12bebf469830894c91fa96a213f8da85576fe1fd
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 12:07:41 +0800

    java

---
+++ JavaOCP_20230715/src/day05/ComboTest.java
@@ -0,0 +1,22 @@
+package day05;
+
+public class ComboTest {
+
+	public static void main(String[] args) {
+		Drink coca = new Drink("可樂", 15);
+		Drink orange = new Drink("柳橙汁", 25);
+		Drink coffee = new Drink("可樂", 35);
+		
+		Bread polo = new Bread("菠羅麵包", 35, 55.5);
+		Bread hotdog = new Bread("熱狗麵包", 50, 78.5);
+		Bread french = new Bread("法國麵包", 75, 30);
+		
+		Cake chocolate = new Cake("巧克力蛋糕", 55, 30);
+		Cake strawberry = new Cake("草莓蛋糕", 45, 40);
+		Cake cheese = new Cake("起司蛋糕", 65, 35);
+		
+		
+
+	}
+
+}
