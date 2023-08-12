32d42e751cbd576a3354726e05f1e1a7a81bd40c
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-12 16:29:08 +0800

    java

--- JavaOCP_20230715/src/day06/FoodTest2.java
+++ JavaOCP_20230715/src/day06/FoodTest2.java
@@ -11,7 +11,7 @@ public class FoodTest2 {
 		SideDish tea = new SideDish("紅茶", 20, true);
 		
 		// 點餐資料
-		Food[] foods = {noodles, rice, soup, tea};
+		Food[] foods = {noodles, rice, soup, tea, noodles};
 		
 		// 印出餐點明細資料
 		displayBillDetails(foods);
@@ -20,22 +20,22 @@ public class FoodTest2 {
 	// 顯示明細
 	public static void displayBillDetails(Food[] foods) {
 		System.out.println("======================= 結帳明細 =======================");
-		System.out.printf("%-20s %-4s\n", "品名", "價格");
+		System.out.printf("%-10s %-4s %-4s %-4s\n", "品名", "單價", "數量", "小計");
 		System.out.println("------------------------------------------------------");
 		
 		int subtotal = 0;
 		for(Food food : foods) {
-			System.out.printf("%-20s $%-4d\n", food.getName(), food.getPrice());
+			System.out.printf("%-10s $%-4d\n", food.getName(), food.getPrice());
 			subtotal += food.getPrice();
 		}
 		System.out.println("------------------------------------------------------");
-		System.out.printf("%-20s $%-4d\n", "小計", subtotal);
+		System.out.printf("%-10s $%-4d\n", "小計", subtotal);
 		
 		int tax = (int)(subtotal * 0.05); // 加上 5% 的稅
-		System.out.printf("%-20s $%-4d\n", "稅", tax);
+		System.out.printf("%-10s $%-4d\n", "稅", tax);
 		
 		int total = subtotal + tax;
-		System.out.printf("%-20s $%-4d\n", "總計", total);
+		System.out.printf("%-10s $%-4d\n", "總計", total);
 		System.out.println("======================================================");
 	}
 	
