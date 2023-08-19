4380820cdcf898990788db8f9b2b06a857cb4180
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-19 14:28:34 +0800

    java

--- JavaOCP_20230715/src/day10/MatchaLatte.java
+++ JavaOCP_20230715/src/day10/MatchaLatte.java
@@ -1,9 +1,16 @@
 package day10;
 
 public class MatchaLatte extends GreenTeaLatte {
- 
-	public void roastMethod() {
+	
+	public MatchaLatte() {
+		setName("抹茶拿鐵");
+		setPrice(75);
 	}
+	
+	public void roastMethod() {
+		System.out.println("加入高級抹茶粉，再使用濃縮咖啡，混合蒸汽牛奶。");
+	}
+	
 	 
 }
  
