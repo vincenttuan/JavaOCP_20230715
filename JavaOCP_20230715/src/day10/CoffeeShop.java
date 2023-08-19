1cda392e823ed3dafb86075752419e71301f87e9
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-19 14:59:16 +0800

    java

--- JavaOCP_20230715/src/day10/CoffeeShop.java
+++ JavaOCP_20230715/src/day10/CoffeeShop.java
@@ -1,6 +1,7 @@
 package day10;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 
 public class CoffeeShop {
@@ -33,7 +34,7 @@ public class CoffeeShop {
 		System.out.printf("最高: %d\n", stat.getMax());
 		System.out.printf("最低: %d\n", stat.getMin());
 		
-		// 印出最高價的咖啡名稱
+		// 印出最高價的咖啡名稱-透過統計數據
 		String highestName = Arrays.stream(coffees)
 								   .filter(coffee -> coffee.getPrice() == stat.getMax()) // 過濾出最高價
 								   .findAny()  // 得到 Optional<Coffee>
@@ -41,6 +42,15 @@ public class CoffeeShop {
 								   .getName(); // 取得 Coffee 名字
 		
 		System.out.printf("最高價的咖啡名稱: %s\n", highestName);
+		
+		// 印出最高價的咖啡名稱-不透過統計數據
+		highestName = Arrays.stream(coffees)
+							.max(Comparator.comparingInt(Coffee::getPrice))  // 取得最高價的 Coffee 物件
+							.map(Coffee::getName)  // 由 Coffee 物件轉換為咖啡名字
+							.orElse("無此咖啡");
+		
+		System.out.printf("最高價的咖啡名稱: %s\n", highestName);
+		
 		
 	}
 
