d50e9bac99953d52fd65c37f7c9588e00e08b0bf
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-19 16:28:31 +0800

    java

--- JavaOCP_20230715/src/day10/combo/meal/ComboB.java
+++ JavaOCP_20230715/src/day10/combo/meal/ComboB.java
@@ -1,5 +1,6 @@
 package day10.combo.meal;
 
+import day10.Cake;
 import day10.ChocolateCake;
 import day10.Latte;
 import day10.combo.Combo;
@@ -9,6 +10,5 @@ public class ComboB extends Combo {
 	public ComboB() {
 		super("B餐", new ChocolateCake(), new Latte());
 	}
-	
 	
 }
