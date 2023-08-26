899ef9bc052bf31a3206d4b52d9cd6d0f0251811
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-26 10:45:43 +0800

    java

--- JavaOCP_20230715/src/day11/CarDemo.java
+++ JavaOCP_20230715/src/day11/CarDemo.java
@@ -5,14 +5,23 @@ public class CarDemo {
 	public static void main(String[] args) {
 		Car car1 = new CUV();
 		Car car2 = new SRV();
+		Car car3 = new Tomico();
+		Car car4 = new ModelCar();
 		
 		car1.start();
 		car2.start();
+		car3.start();
+		car4.start();
+		
 		car1.stop();
 		car2.stop();
+		car3.stop();
+		car3.stop();
 		
 		car1.copyright();
 		car2.copyright();
+		car3.copyright();
+		car4.copyright();
 
 	}
 
