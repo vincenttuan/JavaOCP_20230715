2408236138d03913c0ba1fbc333ba7de1c858e2d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-26 16:00:09 +0800

    JAVA

--- JavaOCP_20230715/src/day12/PredicateDemo1.java
+++ JavaOCP_20230715/src/day12/PredicateDemo1.java
@@ -1,5 +1,7 @@
 package day12;
 
+import java.util.Arrays;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
 public class PredicateDemo1 {
@@ -8,6 +10,11 @@ public class PredicateDemo1 {
 	 * public interface Predicate<T> {
 	 *     boolean test(T t);
 	 * }
+	 * 
+	 * @FunctionalInterface
+	 * public interface IntPredicate {
+     *     boolean test(int value);
+	 * }
 	 * */
 	public static void main(String[] args) {
 		// 判斷分數是否及格 ?
@@ -15,7 +22,13 @@ public class PredicateDemo1 {
 		System.out.println(predicate.test(75));
 		System.out.println(predicate.test(45));
 		
-		
+		// 印出及格的分數
+		int[] scores = {100, 40, 50, 70, 80};
+		IntPredicate pass = score -> score >= 60;
+		Arrays.stream(scores)
+			  //.filter(score -> score >= 60)
+			  .filter(pass)
+			  .forEach(System.out::println);
 
 	}
 
