0189f51713312a36cc5ab444045d58b414485a53
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-26 15:36:49 +0800

    java

---
+++ JavaOCP_20230715/src/day12/FunctionDemo2.java
@@ -0,0 +1,14 @@
+package day12;
+
+public class FunctionDemo2 {
+
+	public static void main(String[] args) {
+		String[] names = {"John", "Alice", "Jo", "Bob", "Vincent"};
+		// 請透過 Function 印出每一個字串的長度
+		
+		
+		// 平均名字有幾個字
+		
+	}
+
+}
