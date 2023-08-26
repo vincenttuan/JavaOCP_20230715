7e1642a9cff97b30757ff2c76ee50be9a77b9701
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-26 13:58:04 +0800

    java

--- JavaOCP_20230715/src/day12/FunctionDemo1.java
+++ JavaOCP_20230715/src/day12/FunctionDemo1.java
@@ -5,15 +5,18 @@ import java.util.function.Consumer;
 public class FunctionDemo1 {
 	/*
 	 * @FunctionalInterface
-	 * 	public interface Consumer<T> {
-     * 		void accept(T t);
+	 * public interface Consumer<T> {
+     *     void accept(T t);
 	 * }
 	 * 
 	 * */
 	public static void main(String[] args) {
+		// 傳統是透過一個 ConsumerImpl 的類來實作 Consumer 介面 
 		Consumer<String> consumer = new ConsumerImpl();
 		consumer.accept("John");
-
+		// Java 8 之後可以透過 Lambda 來簡化實作方法
+		
+		
 	}
 
 }
