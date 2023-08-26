558587d1cfaf2b22efcddbe41e78eb1f532eb436
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-26 10:17:46 +0800

    java

--- JavaOCP_20230715/src/day11/Car.java
+++ JavaOCP_20230715/src/day11/Car.java
@@ -1,9 +1,13 @@
 package day11;
 
 public interface Car {
-	
+	// 抽象方法(待實作的方法)
 	void start(); // public abstract void start();
 	void stop();  // public abstract void stop();
 	
+	// Java 8 之後在 interface 可以透過 default 關鍵字提供有實作的 public 方法
+	default void copyright() {  // public default void copyright() 
+		System.out.println("版權所有");
+	}
 }
  
