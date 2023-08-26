613bca4470cb0b8c4f54a9a2f9c25a38e9c77506
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-26 16:32:24 +0800

    java

---
+++ JavaOCP_20230715/src/day12/SupplierDemo.java
@@ -0,0 +1,40 @@
+package day12;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class SupplierDemo {
+	/*
+	 * @FunctionalInterface
+	 * public interface Supplier<T> {
+	 *     T get();
+	 * }
+	 * 
+	 * */
+	public static void main(String[] args) {
+		Supplier<Date> supplier = () -> new Date();
+		System.out.printf("現在時刻 %s\n", supplier.get());
+		
+		Supplier<Connection> connectionSupplier = () -> {
+			// ... 連接資料庫相關作業
+			return null;
+		};
+		
+		// Supplier<Integer[]> 產生一個 1~39 不重複的號碼共五個
+		// 結果放到 Integer[] 中
+		Supplier<Integer[]> lottoSupplier = () -> {
+			return new Random().ints(1, 40)
+					.distinct()  // 不重複
+					.limit(5)  // 選五個
+					.boxed() // 將 int 轉 Integer
+					.toArray(Integer[]::new);
+		};
+		
+		System.out.println(Arrays.toString(lottoSupplier.get()));
+		
+	}
+
+}
