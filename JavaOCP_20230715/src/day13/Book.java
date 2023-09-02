e3dfd1a3852ec95d7b44be6256a8544b5a8e61bc
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 11:28:26 +0800

    java

--- JavaOCP_20230715/src/day13/Book.java
+++ JavaOCP_20230715/src/day13/Book.java
@@ -1,31 +1,49 @@
 package day13;
 
 public class Book { // 書
-	String name; // 書名
-	BookTag tag;  // 標籤
-	int price; // 價格
+	private String name; // 書名
+	private BookTag tag;  // 標籤
+	private int price; // 價格
 	
-	Book(String name) {
+	public Book(String name) {
 		this.name = name;
 	}
 	
-	Book(String name, int price) {
+	public Book(String name, int price) {
 		this.name = name;
 		this.price = price;
 	}
 	
-	Book(String name, int price, BookTag tag) {
+	public Book(String name, int price, BookTag tag) {
 		this.name = name;
 		this.price = price;
 		this.tag = tag;
 	}
 	
-	void setTag(BookTag tag) { // 設定標籤
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BookTag getTag() {
+		return tag;
+	}
+
+	public void setTag(BookTag tag) {
 		this.tag = tag;
 	}
-	void setPrice(int price) { // 設定價格
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	@Override
 	public String toString() {
 		return String.format("分類: %s 書名: %s 價格: %d", tag, name, price);
