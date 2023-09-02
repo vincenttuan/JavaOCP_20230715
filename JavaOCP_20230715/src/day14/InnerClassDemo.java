fe109b77b2a159210798af4513e388cfcee9f855
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 14:23:26 +0800

    java

--- JavaOCP_20230715/src/day14/InnerClassDemo.java
+++ JavaOCP_20230715/src/day14/InnerClassDemo.java
@@ -1,12 +1,14 @@
 package day14;
 
+import org.apache.commons.math3.ode.ContinuousOutputModel;
+
 class Outer { // 外部類別
 	int x = 7; // Outer 的物件變數
 	class Inner { // 內部類別(不可以有 static 成員)
 		int x = 77;  // Inner 的物件變數
 		void print() { // 物件方法
 			int x = 777;  // 區域變數
-			System.out.println("Hello inner !");
+			System.out.println("Hello 內部類別 !");
 			System.out.println(x); // 777
 			System.out.println(this.x); // 77
 			System.out.println(Outer.this.x); // 7
@@ -15,14 +17,18 @@ class Outer { // 外部類別
 	
 	static class SInner { // 靜態內部類別
 		static void display() { // 類別方法
-			System.out.println("Hello SInner !");
+			System.out.println("Hello 靜態內部類別 !");
 		}
 	}
 	
 	void foo() { // 方法
 		class Demo { // 方法內部類別
-			
+			void show() {
+				System.out.println("Hello 方法內部類別 !");
+			}
 		}
+		Demo demo = new Demo(); // 要自己 new
+		demo.show();
 	}
 	
 	
@@ -38,6 +44,8 @@ public class InnerClassDemo {
 		//-------------------------------------
 		Outer.SInner sin = new Outer.SInner();
 		sin.display();
+		//-------------------------------------
+		ou.foo();
 		
 	}
 
