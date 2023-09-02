bc5a7f7ff811c1dda46abef7c73329669db7cfd6
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 10:02:36 +0800

    java

---
+++ JavaOCP_20230715/src/day13/FunctionDemo.java
@@ -0,0 +1,32 @@
+package day13;
+
+/*
+@FunctionalInterface
+public interface Function<T,R> {
+	R apply(T t);
+}
+
+@FunctionalInterface
+public interface BiFunction<T,U,R> {
+	R apply(T t, U u)
+}
+
+@FunctionalInterface
+public interface BinaryOperator<T> extends BiFunction<T,T,T> {
+	// T apply(T t1, T t2)
+}
+
+@FunctionalInterface
+public interface UnaryOperator<T> extends Function<T,T> {
+	// T apply(T t);
+}
+*/
+
+public class FunctionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
