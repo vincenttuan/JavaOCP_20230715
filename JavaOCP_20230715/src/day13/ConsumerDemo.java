89eb5f4660b985e0403acea76dc3ac86d24e4d89
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 09:58:49 +0800

    java

--- JavaOCP_20230715/src/day13/ConsumerDemo.java
+++ JavaOCP_20230715/src/day13/ConsumerDemo.java
@@ -15,13 +15,18 @@ public interface BiConsumer<T,U> {
 }
 
 */
+
+enum BookTag { // 書籍標籤
+	技術類, 旅遊類, 美食類
+}
+
 class Book { // 書
 	String name; // 書名
-	String tag;  // 標籤
+	BookTag tag;  // 標籤
 	Book(String name) {
 		this.name = name;
 	}
-	void setTag(String tag) { // 設定標籤
+	void setTag(BookTag tag) { // 設定標籤
 		this.tag = tag;
 	}
 	@Override
@@ -34,19 +39,22 @@ public class ConsumerDemo {
 	
 	public static void main(String[] args) {
 		// 標籤機
-		BiConsumer<Book, String> addTagBook = (book, tag) -> book.setTag(tag);
+		BiConsumer<Book, BookTag> addTagBook = (book, tag) -> book.setTag(tag);
 		// 書籍
 		Book book1 = new Book("快快樂樂學程式");
 		Book book2 = new Book("快快樂樂學電腦");
 		Book book3 = new Book("台灣旅遊");
+		Book book4 = new Book("生日蛋糕製作");
 		// 想要針對某一本書貼上標籤分類
-		addTagBook.accept(book1, "技術類");
-		addTagBook.accept(book2, "技術類");
-		addTagBook.accept(book3, "旅遊類");
+		addTagBook.accept(book1, BookTag.技術類);
+		addTagBook.accept(book2, BookTag.技術類);
+		addTagBook.accept(book3, BookTag.旅遊類);
+		addTagBook.accept(book4, BookTag.美食類);
 		// 印出書籍資訊
 		System.out.println(book1);
 		System.out.println(book2);
 		System.out.println(book3);
+		System.out.println(book4);
 	}
 
 }
