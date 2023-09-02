717e8c0cc0edc6d8190d2819aec2e0a76c8c111c
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 16:09:21 +0800

    java

---
+++ JavaOCP_20230715/src/day14/Coin.java
@@ -0,0 +1,15 @@
+package day14;
+
+// 硬幣只有 1, 5, 10, 50
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+}
---
+++ JavaOCP_20230715/src/day14/CoinDemo.java
@@ -0,0 +1,11 @@
+package day14;
+
+public class CoinDemo {
+
+	public static void main(String[] args) {
+		Coin coin = new Coin(5);
+		System.out.printf("得到一個 %d 元硬幣\n" , coin.getValue());
+
+	}
+
+}
