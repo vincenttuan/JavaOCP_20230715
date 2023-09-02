9bf6c726fe71974a17e5bb722b760c1895352c45
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 16:34:58 +0800

    java

--- JavaOCP_20230715/src/day14/TrafficLightDemo.java
+++ JavaOCP_20230715/src/day14/TrafficLightDemo.java
@@ -6,6 +6,7 @@ public class TrafficLightDemo {
 		TrafficLight trafficLight = TrafficLight.黃燈;
 		System.out.println(trafficLight.getDuration());
 		System.out.println(trafficLight);
+		System.out.println(trafficLight.toString());
 		
 		System.out.printf("%s有 %d 秒\n", trafficLight, trafficLight.getDuration());
 
