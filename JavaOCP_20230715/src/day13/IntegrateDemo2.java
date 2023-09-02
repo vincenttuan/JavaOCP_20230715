24aa66d907f179f02479151f232e51f23de214df
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-02 11:53:55 +0800

    java

---
+++ JavaOCP_20230715/src/day13/IntegrateDemo2.java
@@ -0,0 +1,16 @@
+package day13;
+
+// Supplier, BiConsumer, BiPredicate, UnaryOperation (<-Function)
+// 1. Supplier:       提供書籍列表       Supplier<T>       不接受參數, 返回結果的類型是 T
+// 2. BiConsumer:     設定書的標籤       BiConsumer<T, U>  接受二個參數但不返回結果      
+// 3. BiPredicate:    過濾特定的標籤價格   BiPredicate<T, U> 接受二個參數並返回布林值
+// 4. UnaryOperation: 給予書籍特定的折扣   UnaryOperation<T> 接受一個參數與返回結果都是相同類型 T
+
+public class IntegrateDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
