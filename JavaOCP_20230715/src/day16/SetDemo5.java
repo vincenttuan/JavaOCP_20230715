c945a2e998691b39f6091049f2dd9ce0ff2da511
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 16:29:52 +0800

    java

--- JavaOCP_20230715/src/day16/SetDemo5.java
+++ JavaOCP_20230715/src/day16/SetDemo5.java
@@ -1,6 +1,7 @@
 package day16;
 
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class SetDemo5 {
@@ -14,12 +15,23 @@ public class SetDemo5 {
 		scores.add(80);
 		System.out.println(scores); // 80, 90, 100
 		
-		Set<Book> books = new TreeSet<>();
+		TreeSet<Book> books = new TreeSet<>();
 		books.add(new Book("Java", 400));
 		books.add(new Book("Python", 300));
 		books.add(new Book("CPP", 200));
 		books.add(new Book("VB", 250));
+		// 逆序查看-根據 price 由小到大
 		System.out.println(books);
+		
+		// 最貴與最便宜的書
+		System.out.println("最貴的書: " + books.last());
+		System.out.println("最便宜的書: " + books.first());
+		
+		// 逆序查看-根據 price 由大到小
+		System.out.println(books.descendingSet());
+		
+		// 金額大於等於 VB 的書
+		System.out.println(books.tailSet(new Book("VB", 250)));
 		
 		
 	}
