5b9091ac221f7fc40484123f0e17eb5aafc052f1
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 12:12:38 +0800

    java

--- JavaOCP_20230715/src/pattern/observer/Observer.java
+++ JavaOCP_20230715/src/pattern/observer/Observer.java
@@ -3,4 +3,5 @@ package pattern.observer;
 // 觀察者 Observer
 public interface Observer {
 	void update(String message); // 更新通知
+	int getPriority(); // 取得優先權(1..10)
 }
--- JavaOCP_20230715/src/pattern/observer/UserObserver.java
+++ JavaOCP_20230715/src/pattern/observer/UserObserver.java
@@ -3,14 +3,26 @@ package pattern.observer;
 // 訂閱者(就是觀察者 Observer)
 public class UserObserver implements Observer {
 	private String name;
+	private int priority; // 優先權(1~10)
 	
 	public UserObserver(String name) {
 		this.name = name;
+		this.priority = 5;
+	}
+	
+	public UserObserver(String name, int priority) {
+		this.name = name;
+		this.priority = 5;
 	}
 	
 	@Override
 	public void update(String message) {
 		System.out.printf("%s 收到訊息: %s\n", name, message);
+	}
+
+	@Override
+	public int getPriority() {
+		return priority;
 	}
 	
 }
