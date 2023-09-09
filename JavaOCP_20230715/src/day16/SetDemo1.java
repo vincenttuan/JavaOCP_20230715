7cd0e027a3b4e0135397ecf394df56617bed486c
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 14:30:32 +0800

    java

--- JavaOCP_20230715/src/day16/SetDemo1.java
+++ JavaOCP_20230715/src/day16/SetDemo1.java
@@ -12,17 +12,24 @@ public class SetDemo1 {
 		//Set set = new HashSet();       // [80, 100, 90]
 		Set set = new LinkedHashSet();   // [100, 90, 80]
 		//Set set = new TreeSet();       // [80, 90, 100]
-		set.add(100); // int -> Integer
-		set.add(90);  // int -> Integer
-		set.add(Integer.valueOf(80)); // Integer
+		set.add(100); // int -> Integer -> Object
+		set.add(90);  // int -> Integer -> Object
+		set.add(Integer.valueOf(80)); // Integer -> Object
+		set.add(null);
 		System.out.println(set);
 		System.out.println(set.size());
-		// 取得每一個元素
+		
+		// 取得每一個元素並將 null 元素移除
 		Iterator iter = set.iterator(); // 取得集合的尋訪器
 		while (iter.hasNext()) {
 			Object data = iter.next();
+			if(data == null) {
+				iter.remove(); // 移除元素
+				continue;
+			}
 			System.out.println(data);
 		}
+		System.out.println(set);
 		
 		for(Object data : set) {
 			System.out.println(data);
