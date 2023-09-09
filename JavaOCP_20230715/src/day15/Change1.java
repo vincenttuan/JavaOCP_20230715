7502c64620094260c98a3d090b19cf1be658c2f4
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 09:47:18 +0800

    java

---
+++ JavaOCP_20230715/src/day15/Change1.java
@@ -0,0 +1,32 @@
+package day15;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Change1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("商品金額:");
+		int price = new Random().nextInt() + 1; // 1~100
+		System.out.println(price);
+		
+		System.out.print("請輸入付款金額:");
+		int amount = scanner.nextInt();
+		
+		// 計算找零金額
+		int change = amount - price;
+		System.out.printf("找零金額: $%d\n", change);
+		
+		// 計算每個銅板(50, 10, 5, 1)各要找幾個 ?
+		int coin50 = 0;
+		int coin10 = 0;
+		int coin5 = 0;
+		int coin1 = 0;
+		System.out.printf("50元 %d 個\n", coin50);
+		System.out.printf("10元 %d 個\n", coin10);
+		System.out.printf(" 5元 %d 個\n", coin5);
+		System.out.printf(" 1元 %d 個\n", coin1);
+	}
+
+}
