5e252779a8715bba4465a29ddb90bf8ba41ee285
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 10:04:11 +0800

    java

--- JavaOCP_20230715/src/day15/Change1.java
+++ JavaOCP_20230715/src/day15/Change1.java
@@ -20,10 +20,13 @@ public class Change1 {
 		
 		// 計算每個銅板(50, 10, 5, 1)各要找幾個 ?
 		// 可以利用 / 與 % 來計算出
-		int coin50 = 0;
-		int coin10 = 0;
-		int coin5  = 0;
-		int coin1  = 0;
+		int coin50 = change / 50; // 計算出要找幾個 50 元硬幣
+		change = change % 50;     // 找完 50 元後還省下多少錢要找
+		int coin10 = change / 10; // 計算出要找幾個 10 元硬幣
+		change = change % 10;     // 找完 10 元後還省下多少錢要找
+		int coin5  = change / 5;  // 計算出要找幾個 5 元硬幣;
+		change = change % 5;      // 找完 5 元後還省下多少錢要找
+		int coin1  = change;      // 最後剩下的就是 1 元
 		System.out.printf("50元 %d 個\n", coin50);
 		System.out.printf("10元 %d 個\n", coin10);
 		System.out.printf(" 5元 %d 個\n", coin5);
