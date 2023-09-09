cc4d68dd25896998449d7d644201f93872cd6f1d
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 12:02:29 +0800

    java

---
+++ JavaOCP_20230715/src/pattern/observer/Main.java
@@ -0,0 +1,24 @@
+package pattern.observer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 3 個訂閱者(觀察者)
+		Observer observer1 = new UserObserver("John");
+		Observer observer2 = new UserObserver("Mary");
+		Observer observer3 = new UserObserver("Rose");
+		
+		// 1 個被訂閱者(被觀察者)
+		Subject subject = new NewsSubject();
+		
+		// 訂閱
+		subject.add(observer1);
+		subject.add(observer2);
+		subject.add(observer3);
+		
+		// 發送新聞
+		subject.notifyMessage("今天天氣很好~");
+		
+	}
+
+}
