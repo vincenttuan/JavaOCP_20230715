ec4edb4b9809c0782250b6beb8636856bf4fe205
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 10:38:37 +0800

    java

--- JavaOCP_20230715/src/day15/Change3.java
+++ JavaOCP_20230715/src/day15/Change3.java
@@ -1,5 +1,10 @@
 package day15;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import day15.Change2.Coin;
+
 public class Change3 {
 	
 	// 定義一個 Money 這個 enum 來代表各種紙鈔與硬幣的面值
@@ -18,8 +23,46 @@ public class Change3 {
 		}
 	}
 	
+	static class Tx { // 交易
+		private final int price;
+		private int amount;
+		
+		Tx(int price, int amount) {
+			this.price = price;
+			this.amount = amount;
+		}
+		
+		public int getChange() {
+			return amount - price;
+		}
+		
+		void printChangeDetail() {
+			int change = getChange();
+			System.out.printf("找零金額: $%d\n", change);
+			for(Money money : Money.values()) {
+				int count = change / money.getValue();
+				change = change % money.getValue();
+				System.out.printf("%d元 %d 個\n", money.getValue(), count);
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		// 請自行完成
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("商品金額:");
+		int price = new Random().nextInt(100) + 1; // 1~100
+		System.out.println(price);
+		
+		System.out.print("請輸入付款金額:");
+		int amount = scanner.nextInt();
+		
+		//--------------------------------------------------------------
+		
+		// 計算找零金額
+		Tx tx = new Tx(price, amount); // 注入商品價格與付款金額
+		tx.printChangeDetail();
+		
 
 	}
 
