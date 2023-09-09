d8656944c380f0954c456bf9467652d587d8a312
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-09 14:16:54 +0800

    java

--- JavaOCP_20230715/src/day16/EqualsDemo.java
+++ JavaOCP_20230715/src/day16/EqualsDemo.java
@@ -1,5 +1,7 @@
 package day16;
 
+import java.util.Objects;
+
 // 物件內容間的比較
 // 覆寫 equals
 // 覆寫 hashCode
@@ -14,6 +16,23 @@ class Book {
 	}
 	
 	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(name, other.name) && price == other.price;
+	}
+
+	@Override
 	public String toString() {
 		return "Book [name=" + name + ", price=" + price + "]";
 	}
@@ -24,6 +43,10 @@ public class EqualsDemo {
 	public static void main(String[] args) {
 		Book book1 = new Book("Java", 100);
 		Book book2 = new Book("Java", 100);
+		System.out.println(book1);
+		System.out.println(book2);
+		System.out.println(book1.hashCode());
+		System.out.println(book2.hashCode());
 		System.out.println(book1 == book2);
 		System.out.println(book1.equals(book2));
 		
