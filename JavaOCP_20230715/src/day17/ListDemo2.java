5ac11f7f42eb64e55922800d8d4cad15fa587c20
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-16 11:03:14 +0800

    java

--- JavaOCP_20230715/src/day17/ListDemo2.java
+++ JavaOCP_20230715/src/day17/ListDemo2.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-//可變集合/不可變集合/不可修改集合
 /*
+ 可變集合/不可變集合/不可修改集合
+ 
             +-----+-----+--------+
             | add | set | remove |
  +----------+-----+-----+--------+
