043151745712145334d23d75c1f917f7ad7f2c1c
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-16 14:35:19 +0800

    java

---
+++ JavaOCP_20230715/src/day18/MapDemo1.java
@@ -0,0 +1,26 @@
+package day18;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo1 {
+
+	public static void main(String[] args) {
+		Map<String, Integer> map = new LinkedHashMap<>();
+		map.put("國文", 100);
+		map.put("數學", 90);
+		map.put("英文", 80);
+		System.out.println(map);
+		System.out.println(map.keySet());
+		System.out.println(map.values());
+		
+		// Map 中一組(Key/Value)的資料集/元素 稱為 Entry
+		// 很多的 Entry 稱為 EntrySet
+		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry);
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+		}
+	}
+
+}
