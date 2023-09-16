e6cd38ec373e62d605f49c41283b98e8f3f7e117
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-16 10:35:26 +0800

    java

--- JavaOCP_20230715/src/day17/ListDemo1.java
+++ JavaOCP_20230715/src/day17/ListDemo1.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+ * ArrayList: 連續的動態數組,希望能創造O(1)的隨機訪問, 但是需要調整大小結構, 增/刪 (慢)
+ * LinkedList: 元素與元素之間都有參考點, 透過參考形成連續的數組, 隨機訪問比 ArrayList 慢, 但是增/刪 (快)
+ * */
 public class ListDemo1 {
 
 	public static void main(String[] args) {
