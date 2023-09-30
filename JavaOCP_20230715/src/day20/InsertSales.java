4cc9459a434a15b5ae3c8fd5bd44786878ff044e
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-30 16:12:48 +0800

    java

--- JavaOCP_20230715/src/day20/InsertSales.java
+++ JavaOCP_20230715/src/day20/InsertSales.java
@@ -2,7 +2,9 @@ package day20;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class InsertSales {
 	public static void main(String[] args) throws Exception {
@@ -14,7 +16,8 @@ public class InsertSales {
 		int branchId = 2;
 		
 		try(Connection conn = SQLUtil.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement(sql);) {
+			// 加入 Statement.RETURN_GENERATED_KEYS 可以得到最新 id 值	
+			PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
 			
 			pstmt.setString(1, today);
 			pstmt.setInt(2, productId);
@@ -22,7 +25,17 @@ public class InsertSales {
 			pstmt.setInt(4, branchId);
 			
 			int rowcount = pstmt.executeUpdate();
+			
 			System.out.println("新增異動筆數:" + rowcount);
+			
+			// 得到最新 id
+			ResultSet generatedKeys = pstmt.getGeneratedKeys();
+			if(generatedKeys.next()) {
+				int id = generatedKeys.getInt(1);
+				System.out.println("新增成功, 新增的 id = " + id);
+			} else {
+				System.out.println("新增成功, 但無法取得 id");
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
