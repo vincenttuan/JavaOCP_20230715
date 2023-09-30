603041eab867b58f6283b1ca665a8d3a8a2f501e
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-30 13:58:20 +0800

    java

--- JavaOCP_20230715/src/day19/修改資料表.sql
+++ JavaOCP_20230715/src/day19/修改資料表.sql
@@ -1,3 +1,9 @@
--- 修改 branch 資料表alter
+-- 修改 branch 資料表 alter
 -- 將 city + branch_name 的資料具有唯一性
 alter table branch add unique(city, branch_name);
+
+-- 修改 product 資料表 alter
+-- 將 product_name + price 的資料具有唯一性
+alter table product add unique(product_name, price);
+
+
