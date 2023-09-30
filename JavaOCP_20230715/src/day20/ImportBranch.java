bc4d60f911de4912d9c3b2c7e684fc0ed64be4e5
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-30 13:46:11 +0800

    java

--- JavaOCP_20230715/src/day20/ImportBranch.java
+++ JavaOCP_20230715/src/day20/ImportBranch.java
@@ -2,6 +2,10 @@ package day20;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -11,12 +15,27 @@ import java.util.stream.Collectors;
 
 public class ImportBranch {
 	
-	
-	
 	public static void main(String[] args) throws Exception  {
 		Set<Map<String, String>> branches = getBranchSet();
 		// 將 branches 的資料匯入到 mysql branch 資料表中
-		
+		String sql = "insert into branch(city, branch_name) values(?, ?)";
+		try(Connection conn = SQLUtil.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql);) {
+			// 批次新增
+			pstmt.clearBatch(); // 清除批次暫存
+			for(Map<String, String> map : branches) {
+				pstmt.setString(1, map.get("city"));
+				pstmt.setString(2, map.get("branch_name"));
+				pstmt.addBatch(); // 加入到批次
+			}
+			// 執行批次
+			int[] rowscount = pstmt.executeBatch();
+			System.out.println("每一筆的執行回傳效果:" + Arrays.toString(rowscount));
+			System.out.println("共新增筆數:" + Arrays.stream(rowscount).sum());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
