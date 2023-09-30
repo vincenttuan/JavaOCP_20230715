0389c64bca5cb0ee7aa3587b8ab265808a97794b
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-30 12:25:13 +0800

    java

---
+++ JavaOCP_20230715/src/day20/ReadProduct.java
@@ -0,0 +1,53 @@
+package day20;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReadProduct {
+
+	public static void main(String[] args) throws Exception {
+		// 1. 註冊 MySQL(JDBC 4.0 以前)
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		// 2. 連線資料設定
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?serverTimezone=Asia/Taipei";
+		String username = "root";
+		String password = "12345678";
+		
+		// 3. 建立 Connection(連線物件), Statement(SQL語法敘述物件), Result(資料結果集合)
+		String sql = "select product_id, product_name, price from product";
+		
+		try(Connection conn = DriverManager.getConnection(dbUrl, username, password);
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);) {
+			// 4. 將資料表欄位名稱取出
+			ResultSetMetaData md = rs.getMetaData();
+			//System.out.println(md.getColumnCount());
+			//System.out.println(md.getColumnName(1));
+			//System.out.println(md.getColumnName(2));
+			//System.out.println(md.getColumnName(3));
+			for(int i=1;i<=md.getColumnCount();i++) {
+				System.out.printf("%s\t", md.getColumnName(i));
+			}
+			System.out.println("\n---------------------------------------");
+			// 5. 將 rs 資料內容透過輪詢取出
+			while (rs.next()) {
+				int productId = rs.getInt("product_id");
+				String productName = rs.getString("product_name");
+				BigDecimal price = rs.getBigDecimal("price");
+				
+				System.out.printf("%d\t%s\t%s\n", productId, productName, price);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
