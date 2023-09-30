60bafd5a081431cc999e94bb844dbd84275e588d
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-30 11:21:31 +0800

    java

--- JavaOCP_20230715/src/day19/過濾出Branch資料.java
+++ JavaOCP_20230715/src/day19/過濾出Branch資料.java
@@ -7,12 +7,14 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class 過濾出Branch資料 {
 
 	public static void main(String[] args) throws Exception {
 		List<String> lines = Files.readAllLines(Paths.get("src/day18/sales_data.txt"));
 		
+		// 利用 for-loop
 		Set<Map<String, String>> branches = new LinkedHashSet<>();
 		for(int i=1;i<lines.size();i++) {
 			String[] array = lines.get(i).split(",");
@@ -22,7 +24,19 @@ public class 過濾出Branch資料 {
 			branches.add(map);
 			//System.out.println(array[4] + " " + array[5]);
 		}
+		System.out.println(branches);
 		
+		// 利用 Java stream
+		branches = lines.stream()
+				.skip(1) // 跳過首行
+				.map(line -> line.split(","))
+				.map(array -> {
+					Map<String, String> map = new HashMap<>();
+					map.put("city", array[4]);
+					map.put("branch_name", array[5]);
+					return map;
+				})
+				.collect(Collectors.toCollection(LinkedHashSet::new));
 		System.out.println(branches);
 		
 	}
