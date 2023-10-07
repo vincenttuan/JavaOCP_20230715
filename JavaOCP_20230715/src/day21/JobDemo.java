4b934ec16c4d1b0d1c41a28124f3839e908a4f76
MB-teacher <MB-teacher@MB-207-teacher>
2023-10-07 10:18:25 +0800

    java

---
+++ JavaOCP_20230715/src/day21/JobDemo.java
@@ -0,0 +1,13 @@
+package day21;
+
+public class JobDemo {
+
+	public static void main(String[] args) {
+		// 單工運作
+		Job job1 = new Job();
+		Job job2 = new Job();
+		job1.work();
+		job2.work();
+	}
+
+}
