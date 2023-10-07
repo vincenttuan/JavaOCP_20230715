39acd2299277ec7c1a27c22c297d28c4ce726bbb
MB-teacher <MB-teacher@MB-207-teacher>
2023-10-07 14:56:19 +0800

    java

--- JavaOCP_20230715/src/day22/CyclicBarrierDemo2.java
+++ JavaOCP_20230715/src/day22/CyclicBarrierDemo2.java
@@ -3,7 +3,10 @@ package day22;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // 利用 CyclicBarrier 來計算大數據的資料並匯入統一報表
 // 有一個大數據 100, 30, 80 ...
@@ -26,6 +29,11 @@ class AvgThread extends Thread {
 	public void run() {
 		double avg = Arrays.stream(DataCenter.data).average().getAsDouble();
 		DataCenter.report.put("平均業績", (int)avg);
+		try {
+			cb.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
@@ -38,6 +46,11 @@ class HightThread extends Thread {
 	public void run() {
 		int max = Arrays.stream(DataCenter.data).max().getAsInt();
 		DataCenter.report.put("最高業績", max);
+		try {
+			cb.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
@@ -50,6 +63,11 @@ class LowThread extends Thread {
 	public void run() {
 		int min = Arrays.stream(DataCenter.data).min().getAsInt();
 		DataCenter.report.put("最低業績", min);
+		try {
+			cb.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
@@ -62,6 +80,11 @@ class SumThread extends Thread {
 	public void run() {
 		int sum = Arrays.stream(DataCenter.data).sum();
 		DataCenter.report.put("業績總和", sum);
+		try {
+			cb.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
@@ -71,5 +94,15 @@ public class CyclicBarrierDemo2 {
 		Runnable printReport = () -> System.out.println(DataCenter.report);
 		
 		// 請用 CyclicBarrier 來實現 !
+		CyclicBarrier cb = new CyclicBarrier(4, printReport);
+		
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		executor.submit(new AvgThread(cb));
+		executor.submit(new HightThread(cb));
+		executor.submit(new LowThread(cb));
+		executor.submit(new SumThread(cb));
+		
+		executor.shutdown();
+		
 	}
 }
