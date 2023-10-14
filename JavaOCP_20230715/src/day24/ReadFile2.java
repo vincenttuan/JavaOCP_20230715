c118c4a99ad2dba901217026fe943b0a0ba15957
MB-teacher <MB-teacher@MB-207-teacher>
2023-10-14 10:11:55 +0800

    java

---
+++ JavaOCP_20230715/src/day24/ReadFile2.java
@@ -0,0 +1,26 @@
+package day24;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile2 {
+
+	public static void main(String[] args) {
+		// 讀取 src\day24\john_salary.txt
+		String filePath = "src/day24/john_salary.txt";
+		
+		try(FileReader fr = new FileReader(filePath)) {
+			
+			char[] buffer = new char[1024];
+			int charsRead = -1;
+			while((charsRead = fr.read(buffer)) != -1) {
+				System.out.println(buffer);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
